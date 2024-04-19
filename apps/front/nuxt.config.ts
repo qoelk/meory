28781fd399b4467d2f8b9ca63860f1ec267506c2
qoelk <qoelky@gmail.com>
2024-04-20 00:15:31 +0700

    initialise styles, colors, and fonts

--- apps/front/nuxt.config.ts
+++ apps/front/nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    modules: [['@nuxtjs/google-fonts', {
+        families: {
+            'Open+Sans': [400, 600, 700],
+        }
+    }]],
+    devtools: {enabled: true},
+    css: [
+        '@/assets/scss/main.scss'
+    ],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@import "@/assets/scss/_colors.scss";'
+                }
+            }
+        }
+    }
 })
