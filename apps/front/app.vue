de99b4cefadc27bfbc88636e3b55c2859a123ae9
qoelk <qoelky@gmail.com>
2024-04-20 11:19:29 +0700

    boilerplate custom UI components

--- apps/front/app.vue
+++ apps/front/app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <Logo />
+    <CustomInput/>
     <HorizontalEngine />
   </div>
 </template>
---
+++ apps/front/components/CustomInput.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import {defineComponent} from "@vue/runtime-core";
+
+defineComponent({
+  name: 'CustomInput',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<template>
+  <div class="group">
+    <label class="label">Label</label>
+    <input type="text" class="input">
+  </div>
+</template>
+
+<style scoped lang="scss">
+.group {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+  .input {
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin-top: 5px;
+  }
+}
+
+
+</style>
