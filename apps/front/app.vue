cad2f6af2a7146d6d1be54f4adf5f1e99a8e3213
qoelk <qoelky@gmail.com>
2024-06-11 00:46:10 +0700

    boilerplate header and it's components

--- apps/front/app.vue
+++ apps/front/app.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>
-    <Logo />
-    <CustomInput/>
-    <HorizontalEngine />
+  <div class="content">
+    <custom-header/>
+    <horizontal-engine/>
   </div>
 </template>
 <script lang="ts">
+import {defineComponent} from "vue";
+import CustomHeader from "~/components/CustomHeader.vue";
+
+export default defineComponent({
+  components: {CustomHeader}
+})
 </script>
+<style lang="scss">
+.content {
+  margin-top: 120px;
+}
+</style>
